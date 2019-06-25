e5d914a93d140f2adf485878301bce3b8d5f9592
stas <stasek@gmail.com>
2019-06-25 19:32:07 +0300

    Add guest tests for product page

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -13,9 +14,34 @@ from pages.product_page import ProductPage
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_cart(browser, link):
-    page = ProductPage(browser, link)
-    page.open()
+    page = open_product_page(browser, link)
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.success_message_contains_product_name(page.get_product_name())
     page.basket_contains_costs(page.get_product_price())
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = open_product_page(browser, link)
+    page.success_message_is_not_exist()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = open_product_page(browser, link)
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = open_product_page(browser, link)
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+
+def open_product_page(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    return page
