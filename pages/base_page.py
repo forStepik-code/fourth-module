3d97562aa6edfddcfb97ed7fd9279977a3e207f3
stas <stasek@gmail.com>
2019-06-25 20:36:28 +0300

    Add method for checking user login

--- pages/base_page.py
+++ pages/base_page.py
@@ -53,6 +53,11 @@ class BasePage(object):
         link = self.browser.find_element(*BasePageLocators.BASKET)
         link.click()
 
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), \
+            "User icon is not presented," \
+            " probably unauthorised user"
+
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
