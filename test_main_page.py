830ad0291cc9d44d61f3991d6acd293b4b56b122
stas <stasek@gmail.com>
2019-06-25 20:05:08 +0300

    Add tests for checking empty cart page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
-from pages.main_page import MainPage
+from pages.cart_page import CartPage
 from pages.login_page import LoginPage
+from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -23,3 +24,13 @@ def test_guest_can_see_login_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_cart_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_cart_page()
+    cart_page = CartPage(browser, browser.current_url)
+    cart_page.has_empty_items_message()
+    cart_page.is_not_have_items()
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
 import pytest
-from pages.product_page import ProductPage
+
+from pages.cart_page import CartPage
 from pages.login_page import LoginPage
+from pages.product_page import ProductPage
 
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -39,6 +41,15 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = open_product_page(browser, link)
+    page.go_to_cart_page()
+    cart_page = CartPage(browser, browser.current_url)
+    cart_page.has_empty_items_message()
+    cart_page.is_not_have_items()
 
 
 def open_product_page(browser, link):
